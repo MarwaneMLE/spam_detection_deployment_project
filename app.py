57c78e256a153c110fd5d851fe2f856a0ff1fae0
Marwane <khmarwane10@gmail.com>
2023-11-29 12:36:13 +0100

    Spam Detection App-v1

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+#from tokenize import tokenizer
+import pickle
+from tokenize import tokenize
+
+cv = pickle.load(open("models/cv.pkl", "rb"))
+model = pickle.load(open("models/clf.pkl", "rb"))
+
+app = Flask(__name__)
+
+@app.route("/")
+def home(): # Retrieve the entered text
+    return render_template("home.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    if request.method == "POST":
+        email = request.form.get("email-body") # Retrieve the entered text
+    toknized_email = cv.transform([email])
+    predictions = model.predict(toknized_email)
+    if predictions == 1:
+        predictions = 1
+    else:
+        predictions = -1
+    return render_template("home.html", predictions=predictions, email=email)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
